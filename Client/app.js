75c90fbecf77108e2bee43f62f27748cfc587681
mark <markgolah69@gmail.com>
2024-03-27 16:07:01 +0100

    included function to get data

--- Client/app.js
+++ Client/app.js
@@ -5,24 +5,30 @@ const Base_Url = "http://localhost:3000/task";
 
 // $('#submit').on('click', createTask);
 
-$("#form").on("submit" , (e)=>{
-        e.preventDefault();
-        const name = $('#name').val();
-        console.log(name);
-       
-            const todo = {name};
-        
+$("#form").on("submit", (e) => {
+    e.preventDefault();
+    const name = $('#name').val();
+    console.log(name);
 
-        fetch(Base_Url + "/create" , {
-            method: "POST",
-            body: JSON.stringify(todo),
-            headers:{"Content-Type":"application/json"}
-        }).then(res=> console.log(res))
-        .catch(err=> console.log(err))
+    const todo = { name };
+
+
+    fetch(Base_Url + "/create", {
+        method: "POST",
+        body: JSON.stringify(todo),
+        headers: { "Content-Type": "application/json" }
+
+    }).then(res => {
+        if (!res.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return res.json();
+    }).then(data => console.log(data))
+    .catch(err => console.log(err))
 })
 
 
-$('h1').on("click" , ()=>{$('h1').html('hello')})
+$('h1').on("click", () => { $('h1').html('hello') })
 
 // console.log($())
 
--- Server/routes/task.route.js
+++ Server/routes/task.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router(); 
 const taskController = require('../controllers/task.controller');
 
-router.post('/createTask', taskController.createTask);
+router.post('/create', taskController.createTask);
 router.get('/', taskController.getTasks);
 router.get('/:taskId', taskController.getOneTask);
 
