ed047c59eb7be583abe02f0ec52ca5bad936bf44
mark <markgolah69@gmail.com>
2024-03-29 14:58:46 +0100

    another commit

--- Client/app.js
+++ Client/app.js
@@ -1,13 +1,18 @@
-
-
 $(function () {
+
     // Handler for .ready() called.
     const Base_Url = "http://localhost:3000/task";
+    // my variables   
     let tasks = [];
 
-    //  render all tasks in dom =================================================================
-    const renderTasks = () => {
+    
+    let newList = $("<ul></ul>");
+    let completedList = $("<ul></ul>");
+    const completedTasks = []; 
 
+    //  render all tasks in dom =================================================================
+
+    const renderTasks = () => {
         fetch(Base_Url).then(res => {
             if (!res.ok) {
                 throw new Error('Network response was not ok');
@@ -16,79 +21,131 @@ $(function () {
         }).then(data => {
 
             data.map((task) => {
-                return tasks.push(task);
+                tasks.push(task);
             })
             console.log(tasks)
+            return tasks;
 
-            let newList = $("<ul></ul>");
+        }).then((tasks) => {
+            const pendingTasks = [];
+            const completedTasks = [];
             tasks.map((task) => {
-                // newList.append("<input type='checkbox'>"+ task.name + " </input>");
-                newList.append(
-                    "<div class='taskStyle'> <label><input type='checkbox' id='check' value =" + task._id + " >" + task.name + "</label>" +
-                    "<div> <button id='check'> delete</button> <button>edit</button> </div> </div> "
-                );
 
+                if (!task.completed) {
+                    pendingTasks.push(task);
+
+                } else {
+                    completedTasks.push(task);
+                }
+                console.log(completedTasks)
             })
-            $('#tasks').append(newList);
 
+            pendingTasks.map((task) => {
+                newList.append(
+                    `<div class='taskStyle'> <label><input type='checkbox' id='check' value = ${task._id}> ${task.name} </label>
+                    <div> <button id='delete' value="${task._id}"> delete</button> <button id='edit' value="${task._id}">edit</button> </div> </div> `
+                );
+            })
+            $("#pending").append(newList);
+
+            completedTasks.map((completedtask) => {
+                completedList.append(
+                    `<div class='taskStyle'> <label><input type='checkbox' id='check' value ="${completedtask._id}" >${completedtask.name} </label>
+                    <div> <button id='delete' value="${completedtask._id}" > delete</button> <button  id='edit' value="${completedtask._id}">edit</button> </div> </div> `
+                );
+                $('#completed').append(completedList);
+            })
+
+
+            if(pendingTasks.length > 0) {
+                $(".heroSection span.pendingTasks").text(`You have ${pendingTasks.length} tasks pending....`)
+            }else{
+                $(".heroSection span.pendingTasks").text(`You have no pending Tasks....`)
+            }
+
+            if(completedTasks.length > 0) return $(".heroSection span.completedTasks").text(`You have completed ${completedTasks.length} tasks you got it ....`);
+            
         }).catch((err) => console.log(err));
-
     }
     renderTasks();
-
+    console.log(tasks)
     // Form functions =======================================================================
 
-$("#form").on("submit", (e) => {
-    e.preventDefault();
-    const name = $('#name').val();
-    console.log(name);
+    $("#form").on("submit", (e) => {
+        e.preventDefault();
+        const name = $('#name').val();
+        console.log(name);
+        const todo = { name };
 
-    const todo = { name };
+        fetch(Base_Url + "/create", {
+            method: "POST",
+            body: JSON.stringify(todo),
+            headers: { "Content-Type": "application/json" }
 
-
-    fetch(Base_Url + "/create", {
-        method: "POST",
-        body: JSON.stringify(todo),
-        headers: { "Content-Type": "application/json" }
-
-    }).then(res => {
-        if (!res.ok) {
-            throw new Error('Network response was not ok');
+        }).then(res => {
+            if (!res.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return res.json();
+        }).then(data => {
+            console.log(data)
+            $('#tasks ul').remove();
+            tasks = [];
+            // tasks.splice(0, tasks.length);
+            completedTasks.splice(0, completedTasks.length);
+            return renderTasks();
         }
-        return res.json();
-    }).then(data => {
-        console.log(data)
-        $('#tasks ul').remove();
-        tasks = [];
-        return renderTasks();
+        )
+            .catch(err => console.log(err))
+
+        $('#name').val("")
+    })
+
+
+    const deleteTask = (taskId) => {
+
+        fetch(Base_Url + `/delete/${taskId}`, {
+            method: 'DELETE',
+            headers: { "Content-Type": "application/json" },
+        }).then(res => {
+            if (!res.ok) return console.log('response is not okay')
+            else return res.json();
+        }).then(data => console.log(data))
+            .catch(err => console.log(err));
+
     }
-    )
-        .catch(err => console.log(err))
-
-    $('#name').val("")
-})
 
 
-    const deleteTask = () => {
-
-    }
-
-    // set taskCompleted=====================================================================================
+    // set taskCompleted=====================================================================================>
     setTimeout(() => {
 
         $('input[type=checkbox]').on('click',
-        function () {
-            if ($(this).is(':checked')) {
-               const taskId = $(this).val();
-               setTaskCompleted(taskId);
-            } else {
-                console.log("checkbox is not checked")
-            }
-        });
+            function () {
+                if ($(this).is(':checked')) {
+                    $(this).css({'text-decoration':'line-through'});
+                    const taskId = $(this).val();
+                    setTaskCompleted(taskId);
+                } else {
+                    console.log("checkbox is not checked")
+                }
+            });
 
 
-        
+
     }, 1000);
+
+
+    // delete Task ==================================================================================================>
+
+    setTimeout(() => {
+
+        $("#delete").on("click", function (){
+             const value = $(this).attr("value");
+                return deleteTask(value);
+        })
+
+    }, 1000)
+
 
     const setTaskCompleted = (taskId) => {
 
@@ -106,17 +163,48 @@ $("#form").on("submit", (e) => {
     }
 
 
-    
-
-
-
-
-  
-
-
-   
     $('h1').on("click", () => { $('h1').html('hello') })
 
-   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
